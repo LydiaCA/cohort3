54058583131b3202239e6f0980c3e79caf8eeb54
LydiaCA <lydiali.pmp@gmail.com>
2019-10-16 13:29:05 -0600

    moving daily codes to this directory.

---
+++ src/javascript/daily.test.js
@@ -0,0 +1,58 @@
+// When import a function, the function name need to be wrapped in {}.
+//import {assertEquals} from './daily'
+import * as functions from './daily'
+
+/* 
+//Example from the stub file "functions.test.js" in project 01-getting-started
+
+test('Check the sizes', () => {
+    expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
+    expect(functions.size(0)).toBe("small");
+    expect(functions.size(10)).toBe("medium");
+    expect(functions.size(15)).toBe("medium");
+    expect(functions.size(20)).toBe("large");
+    expect(functions.size(2000000)).toBe("large");
+}); */
+
+test ('Compare two parameters', () => {
+    expect(functions.assertEquals("a","b")).toBe(false); // expect(function()).toBe();
+    expect(functions.assertEquals("a","a")).toBe(true);
+    expect(functions.assertEquals(1,2)).toBe(false);
+    expect(functions.assertEquals(2,2)).toBe(true);
+    expect(functions.assertEquals("2",2)).toBe(false);
+    expect(functions.assertEquals("This value","This value")).toBe(true);
+});
+
+// In console, run "npm test daily.test.js"
+
+//Daily exercise 2
+
+
+/*
+    Write a function to format an email based on an array.
+*/
+
+test('email builder from an array', () => {
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
+});
+
+//Daily exercise 3
+/*	
+	Write the function to format an email based on an object / map
+*/
+
+test('email builder from an object / map', () => {
+    const name = { fname: 'first', lname: 'last' };
+    expect(functions.makeEmailObj(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: 'First', lname: 'Last' }))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
+        .toEqual("bill.smith@evolveu.ca");
+});
