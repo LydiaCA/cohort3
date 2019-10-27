fb9c823f2900ae44c2a4334185d46bb2d242958f
LydiaCA <lydiali.pmp@gmail.com>
2019-10-27 12:38:16 -0600

    TDD-fundmantal_2_Full Structure

---
+++ src/javascript/10-25-tdd.js
@@ -0,0 +1,10 @@
+//import functions from "./tdd-01";
+const functions = {
+    helloworld: () => {
+        return "Hello World!";
+    },
+    sum: (input1,input2) => {
+        return input1 + input2;
+    }
+}
+export default functions;
---
+++ src/javascript/10-25-tdd.test.js
@@ -0,0 +1,25 @@
+/* 
+TDD Steps:
+ 1.write the stub (method name, parms, dummy return)
+ 2.write a min code to pass
+ 3.write the minimun code to get the tests to pass
+ 4.another test
+ 5.fail
+ 6.write code
+ 7.continue
+ 8.refactor/repeat for all the functionality you need 
+ a stub has a function name, parameters, return dummy
+*/
+
+import functions from "./10-25-tdd";
+
+test ('hello world',() =>{
+    console.log ("Hello World from test");
+
+});
+ test ('chech the sum',() =>{
+     expect (functions.sum(1,2)).toBe(3);
+ })
+ 
+
+ 
