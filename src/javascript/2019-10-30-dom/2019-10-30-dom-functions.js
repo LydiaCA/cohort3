83a7d95b656f43342be2b565a46b594ee221c614
LydiaCA <lydiali.pmp@gmail.com>
2019-10-30 23:59:31 -0600

    DOM FDD

---
+++ src/javascript/2019-10-30-dom/2019-10-30-dom-functions.js
@@ -0,0 +1,8 @@
+const functions = {
+    HelloWorld:() => {
+        console.log("Hello from DOM functions.");
+        return "Hello from DOM functions."
+    }
+}
+
+export default functions;
---
+++ src/javascript/2019-10-30-dom/2019-10-30-dom-functions.test.js
@@ -0,0 +1,6 @@
+import functions from "./2019-10-30-dom-functions.js"
+
+test("Test for DOM", ()=>{
+    console.log("Hello from DOM test.");
+    functions.HelloWorld();
+})
