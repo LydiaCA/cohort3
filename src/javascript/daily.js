b7e6a420e22d6f3bc837d00c48ba669fd1d9fdcb
LydiaCA <lydiali.pmp@gmail.com>
2019-11-04 00:01:25 -0700

    new daily

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,6 +1,32 @@
 const functions = {
+
+    loopStaffIn: (arrStaff) => {
+        const arrEmail0 = [];
+        for (let staff in arrStaff) {
+            console.log("functions.makeEmailObj[staff] is: " + functions.makeEmailObj(staff));
+            arrEmail0.push(functions.makeEmailObj(staff));
+        }
+        console.log("arrEmail0 is: " + arrEmail0);
+        return arrEmail0;
+    },
+
+    //     const arrEmail = arrStaff.map((item) => {
+    //         functions.makeEmailObj(item)
+    //     }),
+    //     return arrEmail;
+    // },
+
+    /* loopStaff - October 22, 2019 */
+    loopStaff: (arrStaff) => {
+        const arrEmail = arrStaff.map((item) => {
+            return functions.makeEmailObj(item)
+        });
+        console.log("arrEmail in loopStaff is: " + arrEmail);
+        return arrEmail;
+    },
+
+    /* More Array Work - October 16-17 */
     arraySlice: (inputArray, startIndex) => {
-        //console.log(inputArray.slice(startIndex));
         return inputArray.slice(startIndex);
     },
 
@@ -10,26 +36,27 @@ const functions = {
 
     arrayForEach: (inputArray) => {
         let newArr = [];
-        inputArray.forEach(function(item) { newArr.push(item+'-'); });
+        inputArray.forEach(function (item) { newArr.push(item + '-'); });
         return newArr;
     },
 
     arrayMap: (inputArray) => {
-        return inputArray.map(Math.sqrt);
+        return inputArray.map(x => Math.sqrt(x));
     },
 
     arrayReduce: (inputArray) => {
-        let addup = inputArray.reduce(function1, 0);
-        function function1(acc, item, index, inputArray) {
-            return acc + item;
-        }
+        let addup = inputArray.reduce(myFunc);
+        // function function1(acc, item, index, inputArray) {
+        //     return acc - item;
+        // }
 
-        // let addup = inputArray.reduce((acc, item, index, inputArray) => acc + item, 0);
-        
+        function myFunc(total, num) {
+            return total - num;
+        }
         return addup;
     },
 
-    
+
     //=== Prepare for Array Work - October 15, 2019 ===//
 
     /*Let’s dig deeper into arrays. Read the documentation on the following and create examples of each one:
@@ -62,9 +89,9 @@ const functions = {
     arrayDoWhile: (inputArray) => {
         var i = 0;
         do {
-            i ++;
-         } while (i<inputArray.length)
-         return i;
+            i++;
+        } while (i < inputArray.length)
+        return i;
     },
 
     arrayForIn: (inputArray) => {
@@ -72,60 +99,64 @@ const functions = {
         for (var property1 in inputArray) {
             string1 += inputArray[property1];
         }
-        console.log("string1 is: " + string1);
         return string1;
     },
 
-    arrayForOf: (arrayEntities, arrayNames) => {
+    arrayForOf: (arrayEntities) => {
         var string1 = "";
-        for(let eachItem of arrayEntities) {
-            string1 += arrayEntities[eachItem];
+        for (let eachItem of arrayEntities) {
+            string1 += eachItem;
+        };
         return string1;
-        }
     },
 
-//=== makeEmailObj - Oct 11, 2019 ===//
+    arrayMap: (inputArray) => {
+        const newArray = inputArray.map(x => Math.sqrt(x));
+        return newArray;
+    },
 
-makeEmailObj: (name) => {
-        let emailAddress = name.fname.toLowerCase() + "." + name.lname.toLowerCase() + "@evolveu.ca";
-        console.log(" *** In makeEmailObj, email address is: " + emailAddress);
+    //=== makeEmailObj - Oct 11, 2019 ===//
+
+    makeEmailObj: (name) => {
+        console.log("first name is: " + name.fname);
+        let firstName = name.fname.toLowerCase();
+        let lastName = name.lname.toLowerCase();
+        //let emailAddress = name.fname.toLowerCase() + "." + name.lname.toLowerCase() + "@evolveu.ca";
+        let emailAddress = firstName + "." + lastName + "@evolveu.ca";
+        console.log("emailAddress is: " + emailAddress);
         return emailAddress;
     },
 
-//=== makeEmailArr - October 9, 2019 ===//
+    //=== makeEmailArr - October 9, 2019 ===//
 
-// If object to be exported, write "export default ObjectName at the end of the JS"
-// If functions need to be exported, write "export" in front of each function.
+    // If object to be exported, write "export default ObjectName at the end of the JS"
+    // If functions need to be exported, write "export" in front of each function.
 
-makeEmailArr: (name) => {
+    makeEmailArr: (name) => {
         let emailAddress = name[0].toLowerCase() + "." + name[1].toLowerCase() + "@evolveu.ca";
-        console.log(" *** In makeEmailArr, email address is: " + emailAddress);
         return emailAddress;
     },
 
-//=== AssertEquals - October 7, 2019 ===//
-/*	
-	Write the function that will create this output:
+    //=== AssertEquals - October 7, 2019 ===//
+    /*	
+        Write the function that will create this output:
+    
+    *** the two values are not the same:
+        p1--> a
+        p2--> b
+    *** the two values are not the same:
+        p1--> 1
+        p2--> 2
+    *** the two values are not the same:
+        p1--> 2
+        p2--> 2
+    */
 
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are not the same:
-    p1--> 2
-    p2--> 2
-*/
+    // Write the function after this comment ---
 
-// Write the function after this comment ---
-
-assertEquals: (num1, num2) => {
+    assertEquals: (num1, num2) => {
         if (num1 === num2) return true;
         else { //else is not necessary here, should be removed. Later after done coding, review again and remove uncessary else.
-            console.log(" *** the two values are not the same. p1-->" + num1 + ", p2-->" + num2);
-            // console.log("p1-->" + num1);
-            // console.log("p2-->" + num2);
             return false;
         }
     }
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -2,26 +2,85 @@
 //import {assertEquals} from './daily'
 import functions from './daily'
 
+/* loopStaff: in / of - October 24, 2019 */
+//Do the same assignment as the last one using the two forms of the “for” statement. 
+//Create your own tests but use the function names that are provided below.
+/* 
+test('email builder for company01', () => {
+    const staffEmail01 = functions.loopStaffIn(data.staff);
+    // Write your tests here
+    expect(staffEmail01[0])
+        .toEqual("jane.smith@evolveu.ca");
+    //const staffEmail2 = functions.loopStaffOf(data.staff);
+    // Write your tests here
+}); */
+
+
+
+/* loopStaff - October 22, 2019 */
+// Write a function that will take an array and return an array of emails. The new function you are writing today will call makeEmailObj that you have written in a previous exercise. 
+// Copy and paste the code provided and you know the rest of the stick...Test first.
+/*
+    Sample data for the next few exercises.
+*/
+
+const data = {
+    staff: [
+        { fname: "Jane", lname: "Smith", balance: 10 },
+        { fname: "Liam", lname: "Henry", balance: 1000 },
+        { fname: "Emma", lname: "Jones", balance: 1330 },
+        { fname: "Olivia", lname: "Notly", balance: 310 },
+        { fname: "Noah", lname: "Ho", balance: 503 },
+        { fname: "William", lname: "Lee", balance: 520 },
+        { fname: "Benjamin", lname: "Amis", balance: 150 },
+    ],
+    company: "EvolveU",
+    city: "Calgary",
+    prov: "Alberta"
+};
+
+
+/*	
+	Write the function to build email addresses for the company.
+*/
+
+test('email builder for company00', () => {
+    const staffEmail = functions.loopStaff(data.staff);
+    console.log("staffEmail is: " + staffEmail);
+    expect(staffEmail[0])
+        .toEqual("jane.smith@evolveu.ca");
+    expect(staffEmail[3])
+        .toEqual("olivia.notly@evolveu.ca");
+    expect(staffEmail[6])
+        .toEqual("benjamin.amis@evolveu.ca");
+});
+
 test('Check for More Array Work - October 16-17', () => {
-    var cars = ["Honda", "BMW", "Toyota"];
-    var fruit = ["Apple", "Orange", "Lemon", "Banana"];
+    let cars = ["Honda", "BMW", "Toyota"];
+    let fruit = ["Apple", "Orange", "Lemon", "Banana"];
     expect(functions.arraySlice(cars,1)).toEqual(["BMW", "Toyota"]);
     // expect(functions.arraySplice(2, 0, "Pear", "Grape")).toBe(["Apple", "Orange", "Lemon", "Pear", "Grape", "Banana"]);
-    var numbers = [64, 49, 16, 4];
+    let numbers = [64, 49, 16, 4];
     expect(functions.arrayForEach(cars)).toEqual(["Honda-", "BMW-", "Toyota-"]); 
-    expect(functions.arraryMap(numbers)).toEqual([8, 7, 4, 2]);
+    expect(functions.arrayMap(numbers)).toEqual([8, 7, 4, 2]);
 });
 
 test('Check the array basic', () => {
-    var cars = ["Honda", "BMW", "Toyota"];
-    var fruit = ["Apple", "Orange", "Lemon", "Banana"];
+    let cars = ["Honda", "BMW", "Toyota"];
+    let fruit = ["Apple", "Orange", "Lemon", "Banana"];
+    let numbers = [1,2,3,4];
     expect(functions.arrayFor(cars)).toBe(cars.length);
     expect(functions.arrayWhile(fruit)).toBe(fruit.length);
     expect(functions.arrayDoWhile(fruit)).toBe(fruit.length);
     expect(functions.arrayForIn(fruit)).toBe("AppleOrangeLemonBanana");
     expect(functions.arrayForOf(fruit)).toBe("AppleOrangeLemonBanana");
-}
-);
+    // expect(functions.arrayReduce(numbers)).toBe([2,4,6,8]);
+
+    expect(functions.arrayReduce([175, 50, 25])).toBe(100);
+
+    
+    
+});
 
 
 /* 
